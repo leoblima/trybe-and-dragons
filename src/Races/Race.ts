fd420c60f5f8b17a2236479fe045f1893a8b68d8
Leonardo Lima <leoblima00@gmail.com>
2022-09-23 17:03:30 -0300

    Feat: adds abstract class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Race;
