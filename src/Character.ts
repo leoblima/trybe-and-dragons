73445c117173ee009ffa394043c729282f25c721
Leonardo Lima <leoblima00@gmail.com>
2022-09-23 21:01:44 -0300

    Feat: complete creation of class Character, but need lint fixes

--- src/Character.ts
+++ src/Character.ts
@@ -1,8 +1,8 @@
-import Archetype, { Mage } from "./Archetypes";
-import Energy from "./Energy";
-import Fighter from "./Fighter";
-import Race from "./Races";
-import getRandomInt from "./utils";
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
 
 class Character implements Fighter {
   private _race: Race;
@@ -15,25 +15,23 @@ class Character implements Fighter {
   private _energy: Energy;
   private _name: string;
 
-  constructor(name: string, race: Race, archetype: Archetype = new Mage(name)) {
+  constructor(
+    name: string,
+    race?: Race,
+    archetype: Archetype = new Mage(name),
+  ) {
     this._name = name;
-    this._race = race;
-
     this._dexterity = getRandomInt(1, 10);
+    this._race = race || new Elf(name, this._dexterity);
     this._defense = getRandomInt(1, 10);
     this._strength = getRandomInt(1, 10);
-
     this._archetype = archetype;
-    this._maxLifePoints = race.maxLifePoints / 2;
+    this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
     this._energy = {
       type_: archetype.energyType,
       amount: getRandomInt(1, 10),
     };
-
-    if (this._race.maxLifePoints === 99) {
-      this._dexterity = this._race.dexterity;
-    }
   }
 
   get race() {
@@ -61,7 +59,46 @@ class Character implements Fighter {
   }
 
   get energy() {
-    const retEnergy = this._energy;
-    return retEnergy;
+    const { type_, amount } = this._energy;
+    return { type_, amount };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage: number = attackPoints - this._defense;
+    let newLifePoints:number = this._lifePoints;
+
+    if (damage > 0) {
+      newLifePoints = this._lifePoints - damage;
+    }
+    this._lifePoints = newLifePoints;
+    if (newLifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    const increment = getRandomInt(1, 10);
+    this._maxLifePoints += increment;
+    this._strength += increment;
+    this._dexterity += increment;
+    this._defense += increment;
+    this._energy.amount = 10;
+
+    if (this._maxLifePoints >= this.race.maxLifePoints) {
+      this._maxLifePoints = this.race.maxLifePoints;
+    }
+
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  special(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength * getRandomInt(1, this._dexterity));
   }
 }
+
+export default Character;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import Archetype from './Archetypes/Archetype';
-import { EnergyType } from './Energy';
+// import Fighter from './Fighter/Fighter';
+import Character from './Character';
 
-class ArchetypeChild extends Archetype {
-  get energyType(): EnergyType {
-    throw new Error('Method not implemented.');
+const c1 = new Character('');
+const result = () => {
+  let res = true;
+  for (let i = 0; i < 3; i++) {
+    const previousLife = c1.lifePoints;
+    if (previousLife <= 0) break;
+    console.log("l0", previousLife);
+    const life = c1.receiveDamage(10 ** i);
+    console.log(`l${i}`, life);
+    res = 10 ** i > previousLife + c1.defense ? life === -1 : life <= previousLife && life >= previousLife - (10 ** i);
+    if (!res) break;
   }
-}
-const archetype = new ArchetypeChild('ALoha');
-console.log(archetype.cost, archetype.name, archetype.special);
+  return res;
+};
+
+console.log(result());
